45b1c7340180ea45f4b4abed8ef266beb726bc15
Tamerlan <timoxa_musaev@mail.ru>
2021-05-18 16:03:59 +0400

    the end of the day

--- src/components/Music/Popular.vue
+++ src/components/Music/Popular.vue
@@ -1,19 +1,42 @@
 <template>
   <div>
-    <youtube :video-id="videoId" ref="youtube" @playing="playing" id="iframe"></youtube>
-    <button @click="playVideo">play</button>
+    <youtube
+      :video-id="playlist[index].videoId"
+      ref="youtube"
+      @playing="playing"
+      id="iframe"
+    ></youtube>
+    <button @click="playAudio">play</button>
     <button @click="pauseVideo">stop</button>
+    <button @click="nextMusic">Next</button>
+    <button @click="previousMusic">Previous</button>
+    <h4>{{ title }}</h4>
+    <h5>Playlist {{ `${this.index + 1}/ ${this.playlist.length}` }}</h5>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      videoId: "lG0Ys-2d4MA",
+      title: "",
+      index: 0,
+      playlist: [
+        {
+          videoId: "tQ0yjYUFKAE",
+        },
+        {
+          videoId: "lG0Ys-2d4MA",
+        },
+      ],
     };
   },
+  computed: {
+    player() {
+      return this.$refs.youtube.player;
+    },
+  },
   methods: {
-    playVideo() {
+    playAudio() {
       this.player.playVideo();
     },
     pauseVideo() {
@@ -22,15 +45,42 @@ export default {
     playing() {
       console.log(" we are watching!!!");
     },
-  },
-  computed: {
-    player() {
-      return this.$refs.youtube.player;
+    fetchTitle() {
+      fetch(
+        "https://noembed.com/embed?url=https://www.youtube.com/watch?v=" +
+          this.playlist[this.index].videoId
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          this.title = data.title;
+        });
     },
+    nextMusic() {
+      if (this.index >= this.playlist.length - 1) {
+        return (this.index = this.playlist.length - 1);
+      } else {
+        this.index = this.index + 1;
+        console.log(this.index);
+        this.fetchTitle();
+      }
+    },
+    previousMusic() {
+      if (this.index <= 0) {
+        return (this.index = 0);
+      } else {
+        this.index = this.index - 1;
+        this.fetchTitle();
+      }
+    },
+  },
+  mounted() {
+    this.fetchTitle();
   },
 };
 </script>
-<style lang="scss">
+<style>
 #iframe {
   display: none !important;
 }
